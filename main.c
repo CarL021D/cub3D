46627f090195dc6aa69693e19306ec01d0fd6919
Carl Boudard <tcarl771@gmail.com>
2023-07-29 10:56:24 +0200

    save

--- main.c
+++ main.c
@@ -17,6 +17,14 @@ int     main(int ac, char **av)
 	x = 0;
 	while (x < MAP_WIDTH)
 	{
+		member_init(rayC);
+		calculate_step_and_side_dist(rayC);
+		perform_dda(rayC);
+		x++;
+	}
+
+	void	member_init(t_rayC *rayC)
+	{
 		rayC.cameraX = 2 * x / double(MAP_WIDTH) - 1;
 		rayC.dirX = rayC.dirX + rayC.planeX * rayC.cameraX;
 		rayC.dirY = rayC.dirY + rayC.planeY * rayC.cameraX;
@@ -35,25 +43,28 @@ int     main(int ac, char **av)
 
 
 	// rayCulate step
-	if (rayC.rayDirX < 0)
+	void	calculate_step_and_side_dist(t_rayC *rayC)
 	{
-		rayC.stepX = -1;
-		rayC.sideDistX = (rayC.posX - rayC.mapX) * rayC.deltaDistX;
-	}
-	else
-	{
-		rayC.stepX = 1;
-		rayC.sideDistX = (rayC.mapX + 1.0 - rayC.posX) * rayC.deltaDistX;
-	}
-	if (rayC.rayDirY < 0)
-	{
-		rayC.stepY = -1;
-		rayC.sideDistY = (rayC.posY - rayC.mapY) * rayC.deltaDistY;
-	}
-	else
-	{
-		rayC.stepY = 1;
-		cacl.sideDistY = (rayC.mapY + 1.0 - rayC.posY) * rayC.deltaDistY;
+		if (rayC.rayDirX < 0)
+		{
+			rayC.stepX = -1;
+			rayC.sideDistX = (rayC.posX - rayC.mapX) * rayC.deltaDistX;
+		}
+		else
+		{
+			rayC.stepX = 1;
+			rayC.sideDistX = (rayC.mapX + 1.0 - rayC.posX) * rayC.deltaDistX;
+		}
+		if (rayC.rayDirY < 0)
+		{
+			rayC.stepY = -1;
+			rayC.sideDistY = (rayC.posY - rayC.mapY) * rayC.deltaDistY;
+		}
+		else
+		{
+			rayC.stepY = 1;
+			cacl.sideDistY = (rayC.mapY + 1.0 - rayC.posY) * rayC.deltaDistY;
+		}
 	}
 
 
@@ -81,61 +92,44 @@ int     main(int ac, char **av)
 
 
 
-	//perform DDA
-	while (rayC.hit == 0)
+	void	perform_dda(t_rayC *rayC)
 	{
-		//jump to next map square, either in x-direction, or in y-direction
-		if (rayC.sideDistX < rayC.sideDistY)
+		while (rayC.hit == 0)
 		{
-			rayC.sideDistX += rayC.deltaDistX;
-			rayC.mapX += rayC.stepX;
-		  	rayC.side = 0;
+			if (rayC.sideDistX < rayC.sideDistY)
+			{
+				rayC.sideDistX += rayC.deltaDistX;
+				rayC.mapX += rayC.stepX;
+				rayC.side = 0;
+			}
+			else
+			{
+				rayC.sideDistY += rayC.deltaDistY;
+				rayC.mapY += rayC.stepY;
+				rayC.side = 1;
+			}
+			if (rayC.worldMap[rayC.mapX][rayC.mapY] > 0)
+				rayC.hit = 1;
 		}
-		else
-		{
-			rayC.sideDistY += rayC.deltaDistY;
-		  	rayC.mapY += rayC.stepY;
-		  	rayC.side = 1;
-		}
-		//Check if ray has hit a wall
-		if (rayC.worldMap[rayC.mapX][rayC.mapY] > 0)
-			rayC.hit = 1;
 	}
 
+	void	caluclate_ray_dist(t_rayC *rayC)
+	{
+		if(rayC.side == 0)
+			rayC.perpWallDist = (rayC.sideDistX - rayC.deltaDistX);
+		else
+			rayC.perpWallDist = (rayC.sideDistY - rayC.deltaDistY);
+		//Calculate height of line to draw on screen
+		rayC.lineHeight = (int)(MAP_HEIGHT / rayC.perpWallDist);
 
-
-	if(rayC.side == 0)
-		rayC.perpWallDist = (rayC.sideDistX - rayC.deltaDistX);
-	else
-		rayC.perpWallDist = (rayC.sideDistY - rayC.deltaDistY);
-	//Calculate height of line to draw on screen
-	rayC.lineHeight = (int)(MAP_HEIGHT / rayC.perpWallDist);
-
-	//calculate lowest and highest pixel to fill in current stripe
-	rayC.drawStart = -rayC.lineHeight / 2 + MAP_HEIGHT / 2;
-	if(rayC.drawStart < 0)
-		rayC.drawStart = 0;
-	rayC.drawEnd = rayC.lineHeight / 2 + MAP_HEIGHT / 2;
-	if(rayC.drawEnd >= MAP_HEIGHT)
-		rayC.drawEnd = MAP_HEIGHT - 1;
-
-
-
-	// caluclate draw end start
-	if (rayC.side == 0)
-		rayC.perpWallDist = (rayC.sideDistX - rayC.deltaDistX);
-	else
-		rayC.perpWallDist = (rayC.sideDistY - rayC.deltaDistY);
-	//rayCulate height of line to draw on screen
-	rayC.lineHeight = (int)(MAP_HEIGHT / rayC.perpWallDist);
-
-	//rayCulate lowest and highest pixel to fill in current stripe
-	rayC.drawStart = -(rayC.lineHeight) / 2 + MAP_HEIGHT / 2;
-	if (rayC.drawStart < 0)
-		rayC.drawStart = 0;
-	rayC.drawEnd = rayC.lineHeight / 2 + MAP_HEIGHT / 2;
-	if(rayC.drawEnd >= MAP_HEIGHT)
-		rayC.drawEnd = MAP_HEIGHT - 1;
+		//calculate lowest and highest pixel to fill in current stripe
+		rayC.drawStart = -rayC.lineHeight / 2 + MAP_HEIGHT / 2;
+		if(rayC.drawStart < 0)
+			rayC.drawStart = 0;
+		rayC.drawEnd = rayC.lineHeight / 2 + MAP_HEIGHT / 2;
+		if(rayC.drawEnd >= MAP_HEIGHT)
+			rayC.drawEnd = MAP_HEIGHT - 1;
+	}
 
 
 #define KEY_UP 126
