a70246d7ab135ddd55bc083937681a161b1c0b1b
Carl Boudard <tcarl771@gmail.com>
2023-08-12 01:43:01 +0200

    unused func removed

--- rayCastUtils1.c
+++ rayCastUtils1.c
@@ -3,8 +3,8 @@
 void	member_init(t_data *data, t_rayC *rayC)
 {
 	rayC.cameraX = 2 * x / double(MAP_WIDTH) - 1;
-	rayC.dirX = rayC.dirX + rayC.planeX * rayC.cameraX;
-	rayC.dirY = rayC.dirY + rayC.planeY * rayC.cameraX;
+	data.dirX = data.dirX + data.planeX * rayC.cameraX;
+	data.dirY = data.dirY + data.planeY * rayC.cameraX;
 	rayC.mapX = (int)(rayC.posX);
 	rayC.mapY = (int)(rayC.posY);
 	if (rayC.rayDirX == 0)
@@ -18,27 +18,27 @@ void	member_init(t_data *data, t_rayC *rayC)
 }
 
 // rayCulate step
-void	calculate_step_and_side_dist(t_rayC *rayC)
+void	calculate_step_and_side_dist(t_data *data, t_rayC *rayC)
 {
 	if (rayC.rayDirX < 0)
 	{
 		rayC.stepX = -1;
-		rayC.sideDistX = (rayC.posX - rayC.mapX) * rayC.deltaDistX;
+		rayC.sideDistX = (data.posX - rayC.mapX) * rayC.deltaDistX;
 	}
 	else
 	{
 		rayC.stepX = 1;
-		rayC.sideDistX = (rayC.mapX + 1.0 - rayC.posX) * rayC.deltaDistX;
+		rayC.sideDistX = (rayC.mapX + 1.0 - data.posX) * rayC.deltaDistX;
 	}
 	if (rayC.rayDirY < 0)
 	{
 		rayC.stepY = -1;
-		rayC.sideDistY = (rayC.posY - rayC.mapY) * rayC.deltaDistY;
+		rayC.sideDistY = (data.posY - rayC.mapY) * rayC.deltaDistY;
 	}
 	else
 	{
 		rayC.stepY = 1;
-		cacl.sideDistY = (rayC.mapY + 1.0 - rayC.posY) * rayC.deltaDistY;
+		cacl.sideDistY = (rayC.mapY + 1.0 - data.posY) * rayC.deltaDistY;
 	}
 }
 
@@ -65,31 +65,17 @@ void	dda(t_rayC *rayC)
 
 void	ray_dist_init(t_rayC *rayC)
 {
- 	//Calculate height of line to draw on screen
-    rayC.lineHeight = (int)( / rayC.perpWallDist);
-
-	//calculate lowest and highest pixel to fill in current stripe
-    rayC.drawStart = -rayC.lineHeight / 2 + SCREEN_HEIGHT / 2;
-    if (rayC.drawStart < 0)
-		rayC.drawStart = 0;
-    rayC.drawEnd = rayC.lineHeight / 2 + SCREEN_HEIGHT / 2;
-    if (rayC.drawEnd >= SCREEN_HEIGHT)
-		rayC.drawEnd = SCREEN_HEIGHT - 1;
-}
-
-void	caluclate_ray_dist(t_rayC *rayC)
-{
-	if(rayC.side == 0)
+	if (rayC.side == 0)
 		rayC.perpWallDist = (rayC.sideDistX - rayC.deltaDistX);
 	else
 		rayC.perpWallDist = (rayC.sideDistY - rayC.deltaDistY);
 	//Calculate height of line to draw on screen
-	rayC.lineHeight = (int)(MAP_HEIGHT / rayC.perpWallDist);
+	rayC.lineHeight = (int)(SCREEN_HEIGHT / rayC.perpWallDist);
 	//calculate lowest and highest pixel to fill in current stripe
-	rayC.drawStart = -rayC.lineHeight / 2 + MAP_HEIGHT / 2;
-	if(rayC.drawStart < 0)
+	rayC.drawStart = -rayC.lineHeight / 2 + SCREEN_HEIGHT / 2;
+	if (rayC.drawStart < 0)
 		rayC.drawStart = 0;
-	rayC.drawEnd = rayC.lineHeight / 2 + MAP_HEIGHT / 2;
-	if(rayC.drawEnd >= MAP_HEIGHT)
-		rayC.drawEnd = MAP_HEIGHT - 1;
+	rayC.drawEnd = rayC.lineHeight / 2 + SCREEN_HEIGHT / 2;
+	if (rayC.drawEnd >= SCREEN_HEIGHT)
+		rayC.drawEnd = SCREEN_HEIGHT - 1;
 }
