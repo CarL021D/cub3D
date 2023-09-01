1a092da6938eae1f4c0759d30982d03dcea371fe
Carl BOUDARD <caboudar@bess-f4r4s2.clusters.42paris.fr>
2023-09-01 08:37:35 +0200

    TO DO added

--- srcs/rendering/keyHandler.c
+++ srcs/rendering/keyHandler.c
@@ -2,14 +2,16 @@
 
 static void	free_all_and_exit(t_data *data)
 {
-	if (data->tex[0].img != NULL)
-		mlx_destroy_image(data->mlx, data->tex[0].img);
-	if (data->tex[1].img != NULL)
-		mlx_destroy_image(data->mlx, data->tex[1].img);
-	if (data->tex[2].img != NULL)
-		mlx_destroy_image(data->mlx, data->tex[2].img);
-	if (data->tex[3].img != NULL)
-		mlx_destroy_image(data->mlx, data->tex[3].img);
+	// TO DO: - set every single value to NULL or 0; 
+
+	// if (data->tex[0].img != NULL)
+	// 	mlx_destroy_image(data->mlx, data->tex[0].img);
+	// if (data->tex[1].img != NULL)
+	// 	mlx_destroy_image(data->mlx, data->tex[1].img);
+	// if (data->tex[2].img != NULL)
+	// 	mlx_destroy_image(data->mlx, data->tex[2].img);
+	// if (data->tex[3].img != NULL)
+		// mlx_destroy_image(data->mlx, data->tex[3].img);
 	mlx_destroy_window(data->mlx, data->mlx_win);
     mlx_destroy_display(data->mlx);
 	exit(EXIT_SUCCESS);
